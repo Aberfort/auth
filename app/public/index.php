a75c487d17cab0201b8d7528bbf1c9c8872e8644
Serhii Vasyliev <serhii.vasyliev@decimadigital.com>
2021-10-01 11:44:36 +0300

    Add index file and css

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,25 @@
-<?php
-$pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-$query = $pdo->query('SHOW VARIABLES like "version"');
-
-$row = $query->fetch();
-
-echo 'MySQL version:' . $row['Value'];
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+  <link href="../build/css/style.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="login">
+  <h1>Login</h1>
+  <form action="authenticate.php" method="post">
+    <label for="username">
+      <i class="fas fa-user"></i>
+    </label>
+    <input type="text" name="username" placeholder="Username" id="username" required>
+    <label for="password">
+      <i class="fas fa-lock"></i>
+    </label>
+    <input type="password" name="password" placeholder="Password" id="password" required>
+    <input type="submit" value="Login">
+  </form>
+</div>
+</body>
+</html>
